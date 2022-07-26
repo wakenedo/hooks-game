3726218c570edf0653104ecd78fa76f0db1292f4
wakenedo <a_nuner@outlook.com>
2022-07-25 22:15:15 -0300

    Adding Texts - Part 2

--- src/Pages/HowToPlay/HaveFun/index.js
+++ src/Pages/HowToPlay/HaveFun/index.js
@@ -24,10 +24,7 @@ const TextBottom = () => {
                 maxWidth='735px'
                 textAlign='justify'
             >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent blandit nunc sed nunc iaculis interdum. Suspendisse nisi dui, vestibulum vel ornare a, feugiat ut ligula. Vestibulum luctus at elit ut cursus.
-                Quisque luctus efficitur sollicitudin. Nulla fringilla nisl mi, sed consequat velit tincidunt vitae. Etiam quis cursus felis, at iaculis nulla. Aenean eget eleifend tortor. Etiam tristique velit et fermentum aliquet. Vivamus eget magna viverra, cursus risus ac, interdum purus.
-                Curabitur semper imperdiet consequat. Integer gravida tellus neque, ac vehicula nisl maximus in. Vestibulum vitae risus neque.Quisque rutrum tellus id eros varius sagittis. Praesent malesuada justo quis dui porta lacinia. Aliquam rhoncus felis sit amet interdum dignissim. Morbi iaculis cursus risus et faucibus.
-                Ut mattis arcu sed placerat maximus. Nam mollis, risus ac pellentesque faucibus, felis leo mattis orci, in porta diam nunc in mauris. Curabitur et quam non ligula placerat tempus. Sed interdum arcu sit amet odio sagittis, nec auctor purus faucibus.
+               Even if you can never achieve the prized top of the leader boards, the real goal of all of this is to have some fun, for a brief moment, or a long period of hours. Its not a competitive game by design, so just click the buttons gently and enjoy the music if you’re that type of player, or you can be that player that throws its mouse at the wall because he lost a challenge It’s all ok, no one is good at everything. For the firs player, give him some energetics, and for the second, take a deep breath and chill out man. Ok jokes aside, Ok i know they were not that funny. Ok I know maybe they were not funny at all. And that’s the point? Find fun in boring things? Hell no Drop the Ball is a hell of a fun game. Ok, maybe exaggerating things a bit. But no mather what, have fun and enjoy a little bit of a good old Drop The Ball break the sweat making combo after combo or just passively clicking the buttons while you sip your coffee mug or tea. Tea is decent, no offense tea guys.  
             </Typography>
         </Box>
     )

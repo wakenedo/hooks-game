dcf974bbfcc9fd4b11de270d80cd7941d3cb7c42
wakenedo <a_nuner@outlook.com>
2022-07-26 12:44:26 -0300

    Adding Texts - Part Final

--- src/Pages/RoadMap/RoadMapTopText/index.js
+++ src/Pages/RoadMap/RoadMapTopText/index.js
@@ -22,10 +22,7 @@ const RoadMapTopText = () => {
                 maxWidth='1510px'
                 textAlign='justify'
             >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent blandit nunc sed nunc iaculis interdum. Suspendisse nisi dui, vestibulum vel ornare a, feugiat ut ligula. Vestibulum luctus at elit ut cursus.
-                Quisque luctus efficitur sollicitudin. Nulla fringilla nisl mi, sed consequat velit tincidunt vitae. Etiam quis cursus felis, at iaculis nulla. Aenean eget eleifend tortor. Etiam tristique velit et fermentum aliquet. Vivamus eget magna viverra, cursus risus ac, interdum purus.
-                Curabitur semper imperdiet consequat. Integer gravida tellus neque, ac vehicula nisl maximus in. Vestibulum vitae risus neque.Quisque rutrum tellus id eros varius sagittis. Praesent malesuada justo quis dui porta lacinia. Aliquam rhoncus felis sit amet interdum dignissim. Morbi iaculis cursus risus et faucibus.
-                Ut mattis arcu sed placerat maximus. Nam mollis, risus ac pellentesque faucibus, felis leo mattis orci, in porta diam nunc in mauris. Curabitur et quam non ligula placerat tempus. Sed interdum arcu sit amet odio sagittis, nec auctor purus faucibus.
+                OK now allow me to not be all serious, fo r this one at least. Because indeed if we reached the last milestone of this project it means we went there, we climbed the mountain. Guys , we did it, step my step and conquered it, and we even had some chuckles and giggles along they way, which is great of course. And also we had made this experience possible, a community driven game, free to play, pay to support. A satire game. A satire game of gaming, for gamers. Yea it's crazy I know, now that I wrote it down, it definitely sounds like crazy stuff, but I thought that it could work out. We are sure about to figure out, no matter the results if is a meteor rise or a limbo thing it sure was an amazing experience and I appreciate you, no mather where we go from here I thank you for being here. Never forget, break some sweat Drop The Ball. 
             </Typography>
         </>
     )

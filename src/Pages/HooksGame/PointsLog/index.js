16fb603da764df3a7b0c0c339395f340e7b80b69
wakenedo <a_nuner@outlook.com>
2022-08-09 15:39:16 -0300

    PointsLog - Scrolling Implemented

--- src/Pages/HooksGame/PointsLog/index.js
+++ src/Pages/HooksGame/PointsLog/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Typography, Stack } from '@mui/material'
+import { Box, Typography, Stack, TableContainer, Table } from '@mui/material'
 
 const PointsLog = ({ points }) => {
 
@@ -9,7 +9,7 @@ const PointsLog = ({ points }) => {
 
     useEffect(() => {
         pointsLog()
-    }, [points])
+    }, [points,])
 
 
     const pointsLog = () => {
@@ -39,38 +39,48 @@ const PointsLog = ({ points }) => {
             sx={{
                 display: 'flex',
                 flexDirection: 'column',
-                width: '200px',
                 height: '295px',
                 maxHeight: 'fit-content',
                 backgroundColor: '#B2B2B2',
                 borderRadius: '5px',
-            }}>
-            <Typography
-                fontFamily='Mouse Memoirs, sans-serif;'
-                fontSize='25px'
-                textAlign='center'
-                margin='4px auto'
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-            >
+            }}
+        >
+            <TableContainer>
+                <Table
+                    sx={{
+                        height: "max-content"
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            fontFamily='Mouse Memoirs, sans-serif;'
+                            fontSize='25px'
+                            textAlign='center'
+                            margin='4px auto'
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}
+                        >
 
-                Points Log :
-                <Stack>
-                    {arrayOfPointsLog.map((item) => {
-                        return (
-                            <Typography
-                                key={item.id}
-                                fontSize='28px'
-                                fontFamily='Mouse Memoirs, sans-serif;'
-                            >
-                                {item.value}
-                            </Typography>
-                        )
-                    })}
-                </Stack>
-            </Typography>
+                            Points Log :
+                            <Stack>
+                                {arrayOfPointsLog.map((item) => {
+                                    return (
+                                        <Typography
+                                            key={item.id}
+                                            fontSize='28px'
+                                            fontFamily='Mouse Memoirs, sans-serif;'
+                                        >
+                                            {item.value}
+                                        </Typography>
+                                    )
+                                })}
+                            </Stack>
+                        </Typography>
+                    </Box>
+                </Table>
+            </TableContainer>
         </Box>
     )
 }
